06267ec0931fd99676f94f41a08db3dca21d67af
Markus Fuchs <fuchs@sero-systems.de>
2022-08-11 10:31:29 +0200

    allow custom PositionDecoder
    - added interface and factory/supplier for `PositionDecoder`
    - allow to provide a custom `PositionDecoder` to the
      `StatefulModeSDecoder`. Keep backward compatibility by using the
      `StatefulPositionDecoder` as default

---
+++ src/main/java/de/serosystems/lib1090/cpr/PositionDecoderSupplier.java
@@ -0,0 +1,12 @@
+package de.serosystems.lib1090.cpr;
+
+import de.serosystems.lib1090.msgs.ModeSDownlinkMsg;
+
+import java.util.function.Function;
+
+public interface PositionDecoderSupplier extends Function<ModeSDownlinkMsg.QualifiedAddress, PositionDecoder> {
+
+	static PositionDecoderSupplier statefulPositionDecoder() {
+		return address -> new StatefulPositionDecoder();
+	}
+}
