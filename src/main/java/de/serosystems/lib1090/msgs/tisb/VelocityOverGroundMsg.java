dce33cd8fa0695d207e2d06239e09f70202ac6da
Markus Fuchs <fuchs@sero-systems.de>
2026-05-13 19:28:08 +0000

    Apply legal header to all Java sources

--- src/main/java/de/serosystems/lib1090/msgs/tisb/VelocityOverGroundMsg.java
+++ src/main/java/de/serosystems/lib1090/msgs/tisb/VelocityOverGroundMsg.java
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of lib1090.
+ *  Copyright (C) 2026 SeRo Systems GmbH
+ *
+ *  lib1090 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  lib1090 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with de.serosystems.lib1090.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.serosystems.lib1090.msgs.tisb;
 
 import de.serosystems.lib1090.exceptions.BadFormatException;
@@ -7,23 +25,6 @@ import de.serosystems.lib1090.msgs.adsb.AirborneOperationalStatusV2Msg;
 import de.serosystems.lib1090.msgs.modes.ExtendedSquitter;
 
 import java.io.Serializable;
-
-/*
- *  This file is part of de.serosystems.lib1090.
- *
- *  de.serosystems.lib1090 is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  de.serosystems.lib1090 is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with de.serosystems.lib1090.  If not, see <http://www.gnu.org/licenses/>.
- */
 
 /**
  * Decoder for TIS-B velocity message (DO-260B, 2.2.17.3.4).
--- src/test/java/de/serosystems/lib1090/SurfacePositionDecodingTest.java
+++ src/test/java/de/serosystems/lib1090/SurfacePositionDecodingTest.java
@@ -1,3 +1,21 @@
+/*
+ *  This file is part of lib1090.
+ *  Copyright (C) 2026 SeRo Systems GmbH
+ *
+ *  lib1090 is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  lib1090 is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with de.serosystems.lib1090.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.serosystems.lib1090;
 
 import de.serosystems.lib1090.msgs.ModeSDownlinkMsg;
