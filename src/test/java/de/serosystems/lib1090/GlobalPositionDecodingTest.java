147dfedb5c0b86c94e4dbaadfaca4bfdc6e04611
Markus Engel <engel@sero-systems.de>
2026-08-11 22:58:57 +0200

    GlobalPositionDecodingTest: renamed misnamed function

--- src/test/java/de/serosystems/lib1090/GlobalPositionDecodingTest.java
+++ src/test/java/de/serosystems/lib1090/GlobalPositionDecodingTest.java
@@ -39,7 +39,7 @@ public class GlobalPositionDecodingTest {
      * @param ref     reference position, may be null if not surface
      * @param expect  expected decoded position
      */
-    private static void testAirborneTiming(boolean surface, int latEven, int lonEven, int latOdd, int lonOdd, Position ref, Position expect) {
+    private static void testDecode(boolean surface, int latEven, int lonEven, int latOdd, int lonOdd, Position ref, Position expect) {
         CPREncodedPosition cprEven = surface ? CPREncodedPosition.ofSurface(17, false, false, latEven, lonEven, Instant.EPOCH) : CPREncodedPosition.ofAirborne(17, false, latEven, lonEven, Instant.EPOCH);
         CPREncodedPosition cprOdd = surface ? CPREncodedPosition.ofSurface(17, true, false, latOdd, lonOdd, Instant.EPOCH) : CPREncodedPosition.ofAirborne(17, true, latOdd, lonOdd, Instant.EPOCH);
 
@@ -66,8 +66,8 @@ public class GlobalPositionDecodingTest {
     @Test
     void testStraddle() {
         // whether straddling positions are handled properly
-        testAirborneTiming(false, 0x0afd9, 0x0, 0x0d79c, 0x00000, null, null);
-        testAirborneTiming(true, 0x0bf7e, 0x0, 0x15e70, 0x00000, new Position(0., 0., 0.), null);
+        testDecode(false, 0x0afd9, 0x0, 0x0d79c, 0x00000, null, null);
+        testDecode(true, 0x0bf7e, 0x0, 0x15e70, 0x00000, new Position(0., 0., 0.), null);
     }
 
     @Test
@@ -76,22 +76,22 @@ public class GlobalPositionDecodingTest {
         {
             // DXB
             Position expected = new Position(55.3657, 25.2532, 0.);
-            testAirborneTiming(false, 0x06AF1, 0x09C16, 0x04706, 0x04D58, null, expected);
+            testDecode(false, 0x06AF1, 0x09C16, 0x04706, 0x04D58, null, expected);
         }
         {
             // LAX
             Position expected = new Position(-118.4085, 33.9416, 0.);
-            testAirborneTiming(false, 0x1505A, 0x1C43E, 0x12014, 0x06CA5, null, expected);
+            testDecode(false, 0x1505A, 0x1C43E, 0x12014, 0x06CA5, null, expected);
         }
         {
             // SYD
             Position expected = new Position(151.1753, -33.9399, 0.);
-            testAirborneTiming(false, 0x0AFCC, 0x1273D, 0x0E011, 0x0503C, null, expected);
+            testDecode(false, 0x0AFCC, 0x1273D, 0x0E011, 0x0503C, null, expected);
         }
         {
             // SCL
             Position expected = new Position(-70.7858, -33.3928, 0.);
-            testAirborneTiming(false, 0x0DE7B, 0x05658, 0x10DF9, 0x0BB04, null, expected);
+            testDecode(false, 0x0DE7B, 0x05658, 0x10DF9, 0x0BB04, null, expected);
         }
     }
 
@@ -103,31 +103,31 @@ public class GlobalPositionDecodingTest {
             // DXB
             Position expected = new Position(55.3657, 25.2532, 0.);
             Position ref = new Position(30., 80., 0.);
-            testAirborneTiming(true, 0x1ABC2, 0x07058, 0x11C19, 0x13560, ref, expected);
+            testDecode(true, 0x1ABC2, 0x07058, 0x11C19, 0x13560, ref, expected);
         }
         {
             // LAX
             Position expected = new Position(-118.4085, 33.9416, 0.);
             Position ref = new Position(-120., -10., 0.);
-            testAirborneTiming(true, 0x14166, 0x110F9, 0x0804F, 0x1B296, ref, expected);
+            testDecode(true, 0x14166, 0x110F9, 0x0804F, 0x1B296, ref, expected);
         }
         {
             // SYD
             Position expected = new Position(151.1753, -33.9399, 0.);
             Position ref = new Position(120., 10., 0.);
-            testAirborneTiming(true, 0x0BF2E, 0x09CF4, 0x18043, 0x140EF, ref, expected);
+            testDecode(true, 0x0BF2E, 0x09CF4, 0x18043, 0x140EF, ref, expected);
         }
         {
             // SCL
             Position expected = new Position(-70.7858, -33.3928, 0.);
             Position ref = new Position(-110., -10., 0.);
-            testAirborneTiming(true, 0x179ED, 0x1595F, 0x037E4, 0x0EC11, ref, expected);
+            testDecode(true, 0x179ED, 0x1595F, 0x037E4, 0x0EC11, ref, expected);
         }
         {
             // Special cases: Equator vs Poles
-            testAirborneTiming(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., 10., 0.), new Position(0., 0., 0.));
-            testAirborneTiming(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., 60., 0.), new Position(0., 90., 0.));
-            testAirborneTiming(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., -60., 0.), new Position(0., -90., 0.));
+            testDecode(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., 10., 0.), new Position(0., 0., 0.));
+            testDecode(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., 60., 0.), new Position(0., 90., 0.));
+            testDecode(true, 0x00000, 0x00000, 0x00000, 0x00000, new Position(0., -60., 0.), new Position(0., -90., 0.));
         }
     }
 
