580d939e26126e39cddfe399a4898b72cd9fcab8
Markus Engel <engel@sero-systems.de>
2026-08-19 14:48:39 +0200

    fixes in Altitude decoder: added validation, refactoring, fixes #128
    # Conflicts:
    #	src/main/java/de/serosystems/lib1090/decoding/Altitude.java

--- src/main/java/de/serosystems/lib1090/decoding/Altitude.java
+++ src/main/java/de/serosystems/lib1090/decoding/Altitude.java
@@ -26,14 +26,15 @@ public final class Altitude {
     /**
      * This method converts a gray code encoded int to a standard decimal int
      *
-     * @param gray gray code encoded int of length bitlength
-     *             bitlength bitlength of gray code
+     * @param gray gray code encoded integer
      * @return radix 2 encoded integer
      */
-    public static int grayToBin(int gray, int bitlength) {
+    public static int grayToBin(int gray) {
         int result = 0;
-        for (int i = bitlength - 1; i >= 0; --i)
-            result = result | ((((0x1 << (i + 1)) & result) >>> 1) ^ ((1 << i) & gray));
+        while (gray != 0) {
+            result ^= gray;
+            gray >>>= 1;
+        }
         return result;
     }
 
@@ -42,95 +43,36 @@ public final class Altitude {
      * assignment per ICAO Annex 10 Volume IV Appendix to Chapter 3 (SSR Automatic Pressure-Altitude
      * Transmission Code)
      *
-     * @param altitude_code as provided in most Mode S replies (13 bits)
-     * @return altitude in feet
+     * @param altitudeCode as provided in most Mode S replies (13 bits)
+     * @return altitude in feet or null if altitude is not available or has an invalid encoding
      */
-    public static Integer decode13BitAltitude(short altitude_code) {
-        // altitude unavailable
-        if (altitude_code == 0) return null;
+    public static Integer decode13BitAltitude(short altitudeCode) {
+        // altitude not available or not valid
+        if (!valid13BitAltitude(altitudeCode)) return null;
 
-        boolean Mbit = (altitude_code & 0x40) != 0;
-        if (!Mbit) {
-            // deliberately not using #decode13BitQBit for performance
-            boolean Qbit = (altitude_code & 0x10) != 0;
-            if (Qbit) { // altitude reported in 25ft increments
-                int N = (altitude_code & 0x0F) | ((altitude_code & 0x20) >>> 1) | ((altitude_code & 0x1F80) >>> 2);
-                return 25 * N - 1000;
-            } else { // altitude is above 50175ft, so we use 100ft increments
-
-                // it's decoded using the Gillham code
-                int C1 = (0x1000 & altitude_code) >>> 12;
-                int A1 = (0x0800 & altitude_code) >>> 11;
-                int C2 = (0x0400 & altitude_code) >>> 10;
-                int A2 = (0x0200 & altitude_code) >>> 9;
-                int C4 = (0x0100 & altitude_code) >>> 8;
-                int A4 = (0x0080 & altitude_code) >>> 7;
-                int B1 = (0x0020 & altitude_code) >>> 5;
-                int B2 = (0x0008 & altitude_code) >>> 3;
-                int D2 = (0x0004 & altitude_code) >>> 2;
-                int B4 = (0x0002 & altitude_code) >>> 1;
-                int D4 = (0x0001 & altitude_code);
-
-                // this is standard gray code
-                int N500 = grayToBin(D2 << 7 | D4 << 6 | A1 << 5 | A2 << 4 | A4 << 3 | B1 << 2 | B2 << 1 | B4, 8);
-
-                // 100-ft steps must be converted
-                int N100 = grayToBin(C1 << 2 | C2 << 1 | C4, 3) - 1;
-                if (N100 == 6) N100 = 4;
-                if (N500 % 2 != 0) N100 = 4 - N100; // invert it
-
-                return -1200 + N500 * 500 + N100 * 100;
-            }
-        } else return null; // unspecified metric encoding
-    }
-
-    /**
-     * Decode Q bit for altitude code according to ICAO Annex 10 Volume IV §3.1.2.6.5.4, Gillham
-     * pulse assignment per ICAO Annex 10 Volume IV Appendix to Chapter 3 (SSR Automatic
-     * Pressure-Altitude Transmission Code)
-     *
-     * @param altitude_code as provided in most Mode S replies (13 bits)
-     * @return value of the Q bit, null if MBit is set or altitude is not available
-     */
-    public static Boolean decode13BitQBit(short altitude_code) {
-        boolean Mbit = (altitude_code & 0x40) != 0;
-        return !Mbit && ((altitude_code & 0x10) != 0);
-    }
-
-    /**
-     * Decode altitude according to ED-102B §2.2.3.2.3.4.3 Figure 2-5 <br>
-     *
-     * @param altitude_encoded 12 bit encoded altitude
-     * @return altitude in feet
-     */
-    public static Integer decode12BitAltitude(short altitude_encoded) {
-        // In contrast to the decodeAltitude method in {@link de.serosystems.lib1090.msgs.modes.AltitudeReply}, input
-        // does not contain the MBit
-        boolean Qbit = decode12BitQBit(altitude_encoded);
-        int N;
+        boolean Qbit = decode13BitQBit(altitudeCode);
         if (Qbit) { // altitude reported in 25ft increments
-            N = (altitude_encoded & 0xF) | ((altitude_encoded & 0xFE0) >>> 1);
+            int N = (altitudeCode & 0x0F) | ((altitudeCode & 0x20) >>> 1) | ((altitudeCode & 0x1F80) >>> 2);
             return 25 * N - 1000;
         } else { // altitude is above 50175ft, so we use 100ft increments
-
             // it's decoded using the Gillham code
-            int C1 = (0x800 & altitude_encoded) >>> 11;
-            int A1 = (0x400 & altitude_encoded) >>> 10;
-            int C2 = (0x200 & altitude_encoded) >>> 9;
-            int A2 = (0x100 & altitude_encoded) >>> 8;
-            int C4 = (0x080 & altitude_encoded) >>> 7;
-            int A4 = (0x040 & altitude_encoded) >>> 6;
-            int B1 = (0x020 & altitude_encoded) >>> 5;
-            int B2 = (0x008 & altitude_encoded) >>> 3;
-            int D2 = (0x004 & altitude_encoded) >>> 2;
-            int B4 = (0x002 & altitude_encoded) >>> 1;
-            int D4 = (0x001 & altitude_encoded);
+            int C1 = (altitudeCode & 0x1000) >>> 12;
+            int A1 = (altitudeCode & 0x0800) >>> 11;
+            int C2 = (altitudeCode & 0x0400) >>> 10;
+            int A2 = (altitudeCode & 0x0200) >>> 9;
+            int C4 = (altitudeCode & 0x0100) >>> 8;
+            int A4 = (altitudeCode & 0x0080) >>> 7;
+            int B1 = (altitudeCode & 0x0020) >>> 5;
+            int B2 = (altitudeCode & 0x0008) >>> 3;
+            int D2 = (altitudeCode & 0x0004) >>> 2;
+            int B4 = (altitudeCode & 0x0002) >>> 1;
+            int D4 = (altitudeCode & 0x0001);
 
             // this is standard gray code
-            int N500 = grayToBin(D2 << 7 | D4 << 6 | A1 << 5 | A2 << 4 | A4 << 3 | B1 << 2 | B2 << 1 | B4, 8);
+            int N500 = grayToBin(D2 << 7 | D4 << 6 | A1 << 5 | A2 << 4 | A4 << 3 | B1 << 2 | B2 << 1 | B4);
 
             // 100-ft steps must be converted
-            int N100 = grayToBin(C1 << 2 | C2 << 1 | C4, 3) - 1;
+            int N100 = grayToBin(C1 << 2 | C2 << 1 | C4) - 1;
             if (N100 == 6) N100 = 4;
             if (N500 % 2 != 0) N100 = 4 - N100; // invert it
 
@@ -139,12 +81,121 @@ public final class Altitude {
     }
 
     /**
-     * Decode the Q bit for an altitude provided according to ED-102B §2.2.3.2.3.4.3 Figure 2-5 <br>
+     * Decode Q bit for altitude code according to ICAO Annex 10 Volume IV §3.1.2.6.5.4, Gillham
+     * pulse assignment per ICAO Annex 10 Volume IV Appendix to Chapter 3 (SSR Automatic
+     * Pressure-Altitude Transmission Code)
      *
-     * @param altitude_encoded 12 bit encoded altitude
+     * @param altitudeCode as provided in most Mode S replies (13 bits)
+     * @return value of the Q bit, null if MBit is set or altitude is not available
+     */
+    public static Boolean decode13BitQBit(short altitudeCode) {
+        boolean Mbit = (altitudeCode & 0x40) != 0;
+        return !Mbit && ((altitudeCode & 0x10) != 0);
+    }
+
+    /**
+     * Decode altitude according to ED-102B §2.2.3.2.3.4.3 Figure 2-5 <br>
+     * Check if a given 13 bit altitude code is valid.
+     * <ul>
+     *     <li>a code of 0 is invalid</li>
+     *     <li>a code with the M bit set (see {@link #decode13BitQBit(short)}) is invalid (unspecified metric encoding)</li>
+     *     <li>a code without the Q bit set is invalid for certain combinations that are unused</li>
+     * </ul>
+     *
+     * @param altitudeCode as provided in most Mode S replies (13 bits)
+     * @return true if altitude code is considered value, false otherwise
+     */
+    public static boolean valid13BitAltitude(short altitudeCode) {
+        if (altitudeCode == 0) // fast path for explicit "invalid value"
+            return false;
+
+        boolean Mbit = (altitudeCode & 0x40) != 0;
+        if (Mbit) return false;
+
+        if (decode13BitQBit(altitudeCode)) {
+            return true;
+        } else {
+            boolean C1 = (altitudeCode & 0x1000) != 0;
+            boolean C2 = (altitudeCode & 0x0400) != 0;
+            boolean C4 = (altitudeCode & 0x0100) != 0;
+            return (!C1 || !C4) && (C1 || C2 || C4);
+        }
+    }
+
+    /**
+     * Decode altitude according to ED-102B §2.2.3.2.3.4.3 Figure 2-5
+     *
+     * @param altitudeCode 12 bit encoded altitude
+     * @return altitude in feet or null if altitude is not available or has an invalid encoding
+     */
+    public static Integer decode12BitAltitude(short altitudeCode) {
+        // altitude not available or not valid
+        if (!valid12BitAltitude(altitudeCode)) return null;
+
+        // In contrast to the decodeAltitude method in {@link de.serosystems.lib1090.msgs.modes.AltitudeReply}, input
+        // does not contain the MBit
+        boolean Qbit = decode12BitQBit(altitudeCode);
+        int N;
+        if (Qbit) { // altitude reported in 25ft increments
+            N = (altitudeCode & 0xF) | ((altitudeCode & 0xFE0) >>> 1);
+            return 25 * N - 1000;
+        } else { // altitude is above 50175ft, so we use 100ft increments
+            // it's decoded using the Gillham code
+            int C1 = (altitudeCode & 0x800) >>> 11;
+            int A1 = (altitudeCode & 0x400) >>> 10;
+            int C2 = (altitudeCode & 0x200) >>> 9;
+            int A2 = (altitudeCode & 0x100) >>> 8;
+            int C4 = (altitudeCode & 0x080) >>> 7;
+            int A4 = (altitudeCode & 0x040) >>> 6;
+            int B1 = (altitudeCode & 0x020) >>> 5;
+            int B2 = (altitudeCode & 0x008) >>> 3;
+            int D2 = (altitudeCode & 0x004) >>> 2;
+            int B4 = (altitudeCode & 0x002) >>> 1;
+            int D4 = (altitudeCode & 0x001);
+
+            // this is standard gray code
+            int N500 = grayToBin(D2 << 7 | D4 << 6 | A1 << 5 | A2 << 4 | A4 << 3 | B1 << 2 | B2 << 1 | B4);
+
+            // 100-ft steps must be converted
+            int N100 = grayToBin(C1 << 2 | C2 << 1 | C4) - 1;
+            if (N100 == 6) N100 = 4;
+            if (N500 % 2 != 0) N100 = 4 - N100; // invert it
+
+            return -1200 + N500 * 500 + N100 * 100;
+        }
+    }
+
+    /**
+     * Decode the Q bit for an altitude provided according to ED-102B §2.2.3.2.3.4.3 Figure 2-5
+     *
+     * @param altitudeCode 12 bit encoded altitude
      * @return value of the Q bit
      */
-    public static boolean decode12BitQBit(short altitude_encoded) {
-        return (altitude_encoded & 0x10) != 0;
+    public static boolean decode12BitQBit(short altitudeCode) {
+        return (altitudeCode & 0x10) != 0;
+    }
+
+    /**
+     * Check if a given 12 bit altitude code is valid.
+     * <ul>
+     *     <li>a code of 0 is invalid</li>
+     *     <li>a code without the Q bit set (see {@link #decode12BitQBit(short)}) is invalid for certain combinations that are unused</li>
+     * </ul>
+     *
+     * @param altitudeCode 12 bit encoded altitude
+     * @return true if altitude code is considered value, false otherwise
+     */
+    public static boolean valid12BitAltitude(short altitudeCode) {
+        if (altitudeCode == 0) // fast path for explicit "invalid value"
+            return false;
+
+        if (decode12BitQBit(altitudeCode)) {
+            return true;
+        } else {
+            boolean C1 = (altitudeCode & 0x800) != 0;
+            boolean C2 = (altitudeCode & 0x200) != 0;
+            boolean C4 = (altitudeCode & 0x080) != 0;
+            return (!C1 || !C4) && (C1 || C2 || C4);
+        }
     }
 }
